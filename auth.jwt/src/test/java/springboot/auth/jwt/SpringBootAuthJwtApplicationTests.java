ff5440477092967893b4031d0879212758eb0e99
Andres Zelaya <andres.zelaya.d@hotmail.com>
2025-03-14 16:00:26 -0300

    Project upload

---
+++ auth.jwt/src/test/java/springboot/auth/jwt/SpringBootAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.auth.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
